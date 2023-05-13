ad03f0bd74457aeaa33b2b1aa8ac50c0876fa3de
Dani2258 <108611811+Dani2258@users.noreply.github.com>
2023-05-13 00:47:29 -0700

    started

---
+++ JS Practice/index.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+
+// var num = prompt('Enter the Name ')
+// var space = " "
+// for(var i=0;i<num.length;i++){
+//     if(num[i]==' '){
+//         console.log(num[0],num[i+1])
+//     }
+//     for(var i=0;i<space.length;i++){
+//     console.log(space[i+1])
+    
+// }
+
+// }
+// const userInput = "John Doe Smith";
+
+
+// const firstLetter = userInput.charAt(0);
+
+
+// const spaceIndexes = [];
+// for (let i = 0; i < userInput.length; i++) {
+//   if (userInput.charAt(i) === " ") {
+//     spaceIndexes.push(i);
+//   }
+// }
+
+// const lettersAfterSpaces = [];
+// for (let i = 0; i < spaceIndexes.length; i++) {
+//   const index = spaceIndexes[i] + 1;
+//   lettersAfterSpaces.push(userInput.charAt(index));
+// }
+
+
+// console.log(`First letter: ${firstLetter}`);
+// console.log(`Letters after spaces: ${lettersAfterSpaces.join(", ")}`);
+// var numbers = [5,6,58,26,25,48,56,17,25,3,64,28,45,62,95,84,725,485,258,456];
+
+// var  largestNumber = Math.max(...numbers);
+// console.log('Largest Number:', largestNumber);
+
+// var smallestNumber = Math.min(...numbers);
+// console.log('Smallest Number:', smallestNumber);
+// var numbers = [15,25,69,48,25,120,150,480,10,130,265,487,5,02,18,25,95,687,2654,1025];
+// var largestNumber = numbers[0];
+// var smallestNumber = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > largestNumber) {
+//     largestNumber = numbers[i];
+//   }
+//   if (numbers[i] < smallestNumber) {
+//     smallestNumber = numbers[i];
+//   }
+// }
+
+// console.log('Largest Number:', largestNumber);
+// console.log('Smallest Number:', smallestNumber);
+// var numbers = +prompt('Enter the number')
+// var smallestNumber = numbers[0];
+// var largestNumber = numbers[0];
+// for(var i=0;i<numbers.length;i++){
+//     if(numbers[i]>largestNumber){
+//         largestNumber = number[i];
+
+//     }
+//     if(number[i]<smallestNumber){
+//         smallestNumber = number[i]
+        
+//     }
+// }
+// console.log('Largest Number:',largestNumber)
+// console.log('smallestNumber:',smallestNumber)
+
+// var visitorName = prompt("Hello! What is your name?");
+// var greetingMessage = "Welcome to our website, " + visitorName + "!";
+// alert(greetingMessage);
+
+// var messageElement = document.getElementById("message");
+// var message = "Hello, and welcome to my website!";
+// messageElement.textContent = message;
+// Function to generate a random color
+
+  
